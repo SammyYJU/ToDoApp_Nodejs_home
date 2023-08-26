f051b5071eedb2ef0e11dac70c78b28b5d17943c
JINMA <test_git@gmail.com>
2023-08-26 18:22:46 +0900

    mod1

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/api/v1/tasks",taskRoute);
 const start = async () => {
     try{
         await connectDB(process.env.MONGO_URL);
-        app.listen(PORT,console.log("Server start up!"));
+        app.listen(process.env.PORT || PORT,console.log("Server start up!"));
     } catch(err){
         console.log(err);
     }
